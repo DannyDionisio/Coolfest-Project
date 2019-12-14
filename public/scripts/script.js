b17e32f9c7ef909d94d0340cb6b2d7de424f9943
Daniela Dionisio <dannyy.dionisio@gmail.com>
2019-12-14 17:01:40 +0000

    trying import API

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,18 @@
-window.addEventListener('load', () => {
-  console.log('Ironmaker app started successfully!');
-}, false);
+window.addEventListener(
+  "load",
+  () => {
+    console.log("Ironmaker app started successfully!");
+  },
+  false
+);
+
+document.getElementById("recipe").onkeyup = function() {
+  let keyword = document.getElementById("recipe").value;
+  axios
+    .get(
+      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${keyword}&apiKey=4bbde67ea47345b69767d4d3093f0fe5`
+    )
+    .then(result => {
+      console.log(result.data);
+    });
+};
