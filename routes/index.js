4002b22f6072afed438460b97ff6ddba22c1df19
Eduardo Martins <eduard_motta@hotmail.com>
2019-12-17 19:37:49 +0000

    backend read working

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,25 @@
 const { Router } = require("express");
 const router = new Router();
 const axios = require('axios');
+const Event = require("../models/event");
 
 router.get("/", (req, res, next) => {
   res.render("index", { title: "Comeal" });
+});
+
+//--- the list of the events ---
+router.get('/events', (req, res, next) => {
+  console.log("getting events");
+  Event.find()
+  .then(allEventFromDB => { // <- Backend requesting data from Mongo (DB) 
+    console.log("got events", allEventFromDB)
+    res.render('events', { events: allEventFromDB }); 
+    //^ Backend is responding to the front end with the data 
+    // that was got from Mongo
+  })
+  .catch(error => {
+    next(error);
+  });
 });
 
 module.exports = router;
