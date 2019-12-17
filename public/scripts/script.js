5b3cb9d43b95ca5ea6ad798d25ffe9a37c8017fa
Daniela Dionisio <dannyy.dionisio@gmail.com>
2019-12-17 20:42:54 +0000

    api imported to create event

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,14 +5,3 @@ window.addEventListener(
   },
   false
 );
-
-document.getElementById("recipe").onkeyup = function() {
-  let keyword = document.getElementById("recipe").value;
-  axios
-    .get(
-      `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${keyword}&apiKey=4bbde67ea47345b69767d4d3093f0fe5`
-    )
-    .then(result => {
-      console.log(result.data);
-    });
-};
--- routes/events.js
+++ routes/events.js
@@ -1,16 +1,25 @@
-'use strict';
+"use strict";
 
 const express = require("express");
 const router = express.Router();
 const Event = require("../models/event");
+const axios = require("axios");
+
 
 
 //------CRIAR VIEWWW
 
- //--- create events --- 
- router.get('/create-event', (req, res, next) => {
-  res.render('create-event');
- });
+//--- create events ---
+router.get("/create-event", (req, res, next) => {
+  axios
+    .get(
+      "https://api.spoonacular.com/recipes/search?apiKey=4bbde67ea47345b69767d4d3093f0fe5"
+    )
+    .then(result => {
+      res.render("create-event", { recipes: result.data.results });
+    });
+});
+
 router.post("/create-event", (req, res, next) => {
   const { title, date, capacity, place, contacts, recipe, comment } = req.body;
   Event.create({
@@ -22,29 +31,30 @@ router.post("/create-event", (req, res, next) => {
     recipe,
     comment
   })
-    .then(event => {  
+    .then(event => {
       res.redirect("/events");
     })
     .catch(next);
 });
 
-
-// -- edit events -- 
+// -- edit events --
 //GET EDIT EVENTS VIEW
 //POST(?) EDIT EVENTS VIEW FORM
 
 // -- delete event
 
-router.post('/events/edit', (req, res, next) => {
-  const { title, date, capacity, place, contacts, recipe, comment  } = req.body;
-  Event.update({_id: req.query.events_id}, { $set: {title, date, capacity, place, contacts, recipe, comment }})
-  .then((event) => {
-    res.redirect('/events');
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+router.post("/events/edit", (req, res, next) => {
+  const { title, date, capacity, place, contacts, recipe, comment } = req.body;
+  Event.update(
+    { _id: req.query.events_id },
+    { $set: { title, date, capacity, place, contacts, recipe, comment } }
+  )
+    .then(event => {
+      res.redirect("/events");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
-
 
 module.exports = router;
