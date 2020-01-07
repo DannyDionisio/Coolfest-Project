c9a0634766c892f2c7c77bb5c5008c8789296c52
Daniela Dionisio <dannyy.dionisio@gmail.com>
2020-01-07 22:06:34 +0000

    trying fix create event and signup

--- routes/events.js
+++ routes/events.js
@@ -1,15 +1,15 @@
-'use strict';
+"use strict";
 
 const express = require("express");
 const router = express.Router();
 const Event = require("../models/event");
 const axios = require("axios");
-const uploadCloud = require('../config/cloudinary.js');
+const uploadCloud = require("../config/cloudinary.js");
 
 //------CRIAR VIEWWW
 
- //--- create events --- 
- router.get("/create-event", (req, res, next) => {
+//--- create events ---
+router.get("/create-event", (req, res, next) => {
   axios
     .get(
       "https://api.spoonacular.com/recipes/search?apiKey=4bbde67ea47345b69767d4d3093f0fe5"
@@ -19,8 +19,7 @@ const uploadCloud = require('../config/cloudinary.js');
     });
 });
 
-
-router.post("/create-event", uploadCloud.single('photo'), (req, res, next) => {
+/*router.post("/create-event", uploadCloud.single('photo'), (req, res, next) => {
   const { title, date, capacity, place, contacts, recipe, comment } = req.body;
   const imgPath = req.file.url;
   const imgName = req.file.originalname;
@@ -40,25 +39,45 @@ router.post("/create-event", uploadCloud.single('photo'), (req, res, next) => {
       res.redirect("/events");
     })
     .catch(next);
+});*/
+
+router.post("/create-event", (req, res, next) => {
+  const { title, date, capacity, place, contacts, recipe, comment } = req.body;
+
+  console.log("title", req.body.title);
+  Event.create({
+    title,
+    date,
+    capacity,
+    place,
+    contacts,
+    recipe,
+    comment
+  })
+    .then(event => {
+      res.redirect("/events");
+    })
+    .catch(next);
 });
 
-
-// -- edit events -- 
+// -- edit events --
 //GET EDIT EVENTS VIEW
 //POST(?) EDIT EVENTS VIEW FORM
 
 // -- delete event
 
-router.post('/events/edit', (req, res, next) => {
-  const { title, date, capacity, place, contacts, recipe, comment  } = req.body;
-  Event.update({_id: req.query.events_id}, { $set: {title, date, capacity, place, contacts, recipe, comment }})
-  .then((event) => {
-    res.redirect('/events');
-  })
-  .catch((error) => {
-    console.log(error);
-  })
+router.post("/events/edit", (req, res, next) => {
+  const { title, date, capacity, place, contacts, recipe, comment } = req.body;
+  Event.update(
+    { _id: req.query.events_id },
+    { $set: { title, date, capacity, place, contacts, recipe, comment } }
+  )
+    .then(event => {
+      res.redirect("/events");
+    })
+    .catch(error => {
+      console.log(error);
+    });
 });
-
 
 module.exports = router;
