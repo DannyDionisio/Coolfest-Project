e5d57fc46ea32bcf7d513a7dd2d36c79d8ee4f86
Daniela Dionisio <dannyy.dionisio@gmail.com>
2020-01-11 16:28:10 +0000

    add mongo from heroku

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const MongoStore   = require('connect-mongo')(session);
 
 
 mongoose
-  .connect('mongodb://localhost/coolfest-database', {useNewUrlParser: true})
+  .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
